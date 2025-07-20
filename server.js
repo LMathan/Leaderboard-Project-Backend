1b5053b2e8db1478c89bd51a5eaf4a48edc9344e
L.Mathan <125793554+LMathan@users.noreply.github.com>
2025-07-20 10:14:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use('/api', userRoutes);
 
 const PORT = process.env.PORT || 5000;
-mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => {
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 })
